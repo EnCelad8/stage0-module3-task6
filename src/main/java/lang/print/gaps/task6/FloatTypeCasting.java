b2f25818ccf2a583bd051aa1ae03b079ee761476
EnCelad8 <naxmax70@gmail.com>
2023-11-16 15:32:47 +0300

    changes

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.task6;
 
+import java.util.Map;
+
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int result = (int) Math.round(numberToBeRounded);
+        System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/task6/PrePostIncrement.java
+++ src/main/java/lang/print/gaps/task6/PrePostIncrement.java
@@ -6,7 +6,7 @@ public class PrePostIncrement {
         int b = 7;
         int c = 12;
         int d = 15;
-
+        a++;
         // use post-increment operator in the mathematical expression  
         int x =   a   + b +   c   + d;
         System.out.println("Expected x is 40, x = " + x);
